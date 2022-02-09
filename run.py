3c1a5eea19f22f31199bb1daecfba2751d5c33e0
lauraz-15 <laura.zvirbule90@gmail.com>
2022-02-09 19:49:30 +0000

    Add get_user_steps function and test it

--- run.py
+++ run.py
@@ -15,6 +15,22 @@ SHEET = GSPREAD_CLIENT.open('WalkActive')
 
 sales = SHEET.worksheet('steps')
 
-data = sales.get_all_values()
+# data = sales.get_all_values()
 
-print(data)
+# print(data)
+
+def get_user_steps():
+    """
+    Get daily steps from the user for the past 7 days
+    """
+    print("Please enter the daily step count for the past 7 days\n")
+    print("The numbers must be entered in the the following format")
+    print("7 numbers seperated by commas")
+    print("Example: 100, 2000, 33, 777, 8585, 6456, 1456\n")
+
+    user_steps = input("Enter your daily steps here: \n")
+
+    print(f"You have entered: {user_steps}")
+
+
+get_user_steps()
